70259c587ef6f057402dc1c153ce6b6f57ced920
k-szulc <s-q-n@protonmail.com>
2021-10-21 17:45:59 +0200

    moved properties to application.properties

--- src/main/java/xyz/itbs/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/xyz/itbs/sfgdi/config/GreetingServiceConfig.java
@@ -9,7 +9,6 @@ import xyz.itbs.sfgdi.repositories.EnglishGreetingRepository;
 import xyz.itbs.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import xyz.itbs.sfgdi.services.*;
 
-@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
